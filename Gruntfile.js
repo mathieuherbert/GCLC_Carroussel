2493ede9a141c3144ec9f2744aff75908529716a
mcartaud <mathieu.cartaud@gmail.com>
2014-11-26 22:07:57 +0100

    add files for build

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,19 +5,37 @@ module.export = function (grunt) {
 
   grunt.initConfig({
     uglify: {
+      options: {
+        banner: '/*! (c) it-gems.com 2014 Filanthropes */\n'
+      },
       build: {
-        files: [{
-          expand: true,
-          src: '**/*.js',
-          dest: 'dest/js',
-          cwd: '.'
-        }]
+        src: ['<%= pkg.srcDir %>/scrollspy.js', '<%= pkg.srcDir %>/init.js' ], //Use a comma-separated list of the JS files you want to minify
+        dest: '<%= pkg.srcDir %>/built.min.js'
+      }
+    },
+
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/* CSS minified (c) Filanthropes 2014 */'
+        },
+        files: {
+          'css/*.min.css': ['css/*.css'] //The list of CSS files to minify
+        }
       }
     }
   });
 
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  //Load the plugin to minify CSSs
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
   grunt.registerTask('build', [
-    'uglify'
+    'uglify',
+    'cssmin'
   ]);
 
 };
