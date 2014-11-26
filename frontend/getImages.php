55844d2526cf6250a63e5068b75b38284f22965a
math.herbert <math.herbert@gmail.com>
2014-11-27 00:12:23 +0100

    add getImages

--- frontend/getImages.php
+++ frontend/getImages.php
@@ -1,7 +1,24 @@
 <?php
+    $files = scandir ( "../img/slideshow");
+
+
+   
+
+    $shortFiles = array();
+    foreach($files as $file){
+        if($file == ".." || $file == "."){
+            continue;
+        }
+        if(!(substr( $file, 0, 3 ) == "big")){
+            break;
+        }
+        array_push($shortFiles,substr($file, 3));
+
+    }
     $ret = array(
-        "size" => 2,
-        "files" => array("Img1.gif", "Img2.gif")
+        "size" => count($shortFiles),
+        "files" => $shortFiles,
 
     );
+
     echo json_encode($ret);
