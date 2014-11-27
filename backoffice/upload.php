b0e59db2df104f43b808cb4a0cfd54c9cf43bf9d
Julien DERAY <julien@deray.fr>
2014-11-27 18:47:44 +0100

    add test to know if GD is installed

--- backoffice/upload.php
+++ backoffice/upload.php
@@ -41,6 +41,11 @@
 <?php
 ini_set('display_errors', 'on');
 
+
+$testGD = get_extension_funcs("gd"); // Grab function list
+if (!$testGD){ echo "GD not even installed."; exit; }
+echo"<pre>".print_r($testGD,true)."</pre>";
+
 if(isset($_FILES["upload"]["tmp_name"])){
     list($width, $height) = getimagesize($_FILES["upload"]["tmp_name"]);
     $newWidth1 = 900;
