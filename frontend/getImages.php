773b881d5e8d7990aca90f1cb68e7c69682bc2f7
math.herbert <math.herbert@gmail.com>
2014-11-27 01:11:34 +0100

    getImages with comments

--- frontend/getImages.php
+++ frontend/getImages.php
@@ -1,18 +1,26 @@
 <?php
     $files = scandir ( "../img/slideshow");
-
-
-
-
     $shortFiles = array();
     foreach($files as $file){
         if($file == ".." || $file == "."){
             continue;
         }
         if(!(substr( $file, 0, 3 ) == "big")){
-            break;
+            continue;
         }
-        array_push($shortFiles,substr($file, 3));
+        $file_name = substr($file, 3);
+        $file_prop_name = substr($file_name,0, strrpos($file_name,"."));
+        $file_prop = new SplFileObject("../img/slideshow/".$file_prop_name.".prop");
+        $comment = "";
+        while (!$file_prop->eof()) {
+            $line = $file_prop->fgets();
+            if(substr($line,0,11) == "Description"){
+                $comment = rtrim(substr($line, trim(strpos($line,"=")+1)));
+                break;
+            }
+        }
+
+        array_push($shortFiles,array($file_name => $comment));
 
     }
     $ret = array(
