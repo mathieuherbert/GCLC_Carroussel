c9d5f19fe8539d28bf529d25bd3406444288da53
mcartaud <mathieu.cartaud@gmail.com>
2014-11-27 01:17:02 +0100

    finish build js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,82 @@
-'use strict';
 
-module.export = function (grunt) {
+module.exports = function (grunt) {
 
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
+
     uglify: {
       options: {
         banner: '/*! (c) it-gems.com 2014 Filanthropes */\n'
       },
-      build: {
-        src: ['js/jquery.js', 'js/responsiveImage.js', 'js/slideshow.js' ], //Use a comma-separated list of the JS files you want to minify
-        dest: '<%= pkg.srcDir %>/built.min.js'
+      my_target: {
+        files: [{
+          expand: true,
+          cwd: 'js',
+          src: '**/*.js',
+          dest: 'dest/js'
+        }]
       }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeCommentsFromCDATA: true,
+          removeOptionalTags: true
+        },
+        files: [{
+          expand: true,
+          src: ['index.html'],
+          dest: 'dest'
+        }]
+      }
+    },
+
+    copy: {
+      main: {
+        files: [
+        // Copies files from the source folder to your local Apache folder
+        {expand: true, src: ['css/*.min.css'],
+        dest: 'dest/'},
+        {expand: true, src: ['img/*'],
+        dest: 'dest/', filter: 'isFile'},
+        {expand: true, src: ['img/**'],
+        dest: 'dest/'}
+        ],
+      },
     },
 
     cssmin: {
       add_banner: {
         options: {
-          banner: '/* CSS minified (c) Filanthropes 2014 */'
+          banner: '/* CSS minified (c) Filanthropes 2014 */ \n'
         },
         files: {
-          'css/*.min.css': ['css/*.css'] //The list of CSS files to minify
+          'dest/css/gclc.css': ['css/gclc.css'] //The list of CSS files to minify
         }
       }
     }
   });
 
+  //Load the plugin that copies files and directories
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
   //Load the plugin to minify CSSs
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.loadNpmTasks('load-grunt-tasks');
-
   grunt.registerTask('default', [
-    'uglify',
-    'cssmin'
+  'uglify',
+  'htmlmin',
+  'cssmin',
+  'copy'
   ]);
 
 };
--- dest/readme.md
+++
@@ -1,1 +0,0 @@
-a simple file to have this folder sync on github
