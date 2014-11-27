c835e24b87b4ed74f2204256ca20482d25c8bfc6
mcartaud <mathieu.cartaud@gmail.com>
2014-11-27 22:28:53 +0100

    add swipe

--- js/slideshow.js
+++ js/slideshow.js
@@ -113,6 +113,52 @@ function prev(){
   fullMajImages();
 }
 
+function detectswipe(el,func) {
+  swipe_det = new Object();
+  swipe_det.sX = 0;
+  swipe_det.sY = 0;
+  swipe_det.eX = 0;
+  swipe_det.eY = 0;
+  var min_x = 20;  //min x swipe for horizontal swipe
+  var max_x = 40;  //max x difference for vertical swipe
+  var min_y = 40;  //min y swipe for vertical swipe
+  var max_y = 50;  //max y difference for horizontal swipe
+  var direc = "";
+  ele = document.getElementById("slideshow");
+  ele.addEventListener('touchstart',function(e){
+    var t = e.touches[0];
+    swipe_det.sX = t.screenX;
+    swipe_det.sY = t.screenY;
+  },false);
+  ele.addEventListener('touchmove',function(e){
+    e.preventDefault();
+    var t = e.touches[0];
+    swipe_det.eX = t.screenX;
+    swipe_det.eY = t.screenY;
+  },false);
+  ele.addEventListener('touchend',function(e){
+    //horizontal detection
+    if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
+      if(swipe_det.eX > swipe_det.sX) direc = "r";
+      else direc = "l";
+    }
+
+    if (direc != "") {
+      if(typeof func == 'function') func(el,direc);
+    }
+    direc = "";
+  },false);
+}
+
+function launchSwipe(el,d) {
+  if (d == "l") {
+    next();
+  } else if (d == "r") {
+    prev();
+  }
+}
+
+
 $(document).ready(function(){
   function test(){
      $typeScreen = screen.width >= 750 ;
@@ -143,6 +189,7 @@ $(document).ready(function(){
       }
     });
 
+    detectswipe('slides',launchSwipe);
 
 }
   loadImage();
