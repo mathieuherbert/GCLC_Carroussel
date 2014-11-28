eee2ae5dedf9c5c44f8f77af909e46fe5cfc88a4
mcartaud <mathieu.cartaud@gmail.com>
2014-11-28 10:24:51 +0100

    change image extension to save place

--- js/slideshow.js
+++ js/slideshow.js
@@ -268,8 +268,6 @@ $(document).ready(function(){
 
 });
 
-var addEvent=function(){return document.addEventListener?function(a,c,d){if(a&&a.nodeName||a===window)a.addEventListener(c,d,!1);else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}:function(a,c,d){if(a&&a.nodeName||a===window)a.attachEvent("on"+c,function(){return d.call(a,window.event)});else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}}();
-
 
 isPlaying = true;
 var indexImage=0;
